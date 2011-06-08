1110c1d5afef5c431bb9295396abf73593ace854
oleg dashevskii <olegdashevskii@gmail.com>
2011-06-08 12:03:58 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playriak::Application.config.secret_token = 'cb84f40bcd4c8a46202a68b07866eebacd0c7466239d229d4bcc3448a9dabac8af7566449cfe47a94bf66a2aeac3877be1ee6b84be01f33ca18d50bc189158ba'
